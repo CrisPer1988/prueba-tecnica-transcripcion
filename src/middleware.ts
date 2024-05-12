0c577473fec5b35b50438fe2fe441927990604f5
CristianPerez <norberto.cp@hotmail.com>
2024-05-12 12:19:38 -0300

    change cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,57 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-const allowdOrigins =
-  process.env.NODE_ENV === "production"
-    ? [
-        "https://prueba-tecnica-transcripcion.vercel.app/",
-        "https://www.prueba-tecnica-transcripcion.vercel.app",
-      ]
-    : ["http://localhost:3000"];
+import { NextRequest, NextResponse } from "next/server";
 
-export default function middlewate(request: Request) {
-  console.log("ENTRANDO AL MIDDLEWARE");
+// const allowdOrigins =
+//   process.env.NODE_ENV === "production"
+//     ? [
+//         "https://prueba-tecnica-transcripcion.vercel.app/",
+//         "https://www.prueba-tecnica-transcripcion.vercel.app",
+//       ]
+//     : ["http://localhost:3000"];
 
-  const origin = request.headers.get("origin");
-  console.log(origin);
+// export default function middlewate(request: Request) {
+//   console.log("ENTRANDO AL MIDDLEWARE");
 
-  if (origin && !allowdOrigins.includes(origin)) {
-    return new NextResponse(null, {
-      status: 400,
-      statusText: "Bad request",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-    });
-  }
+//   const origin = request.headers.get("origin");
+//   console.log(origin);
 
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+//   if (origin && !allowdOrigins.includes(origin)) {
+//     return new NextResponse(null, {
+//       status: 400,
+//       statusText: "Bad request",
+//       headers: {
+//         "Content-Type": "text/plain",
+//       },
+//     });
+//   }
 
-  return response;
-}
+//   const response = NextResponse.next();
+//   response.headers.set("Access-Control-Allow-Origin", "*");
+//   response.headers.set(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+//   return response;
+// }
 
 // export const config = {
 //   matcher: "/api/:path*",
 // };
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  res.headers.append(
+    "ACCESS-CONTROL-ALLOW-ORIGIN",
+    "https://prueba-tecnica-transcripcion-cristians-projects-99e3cf7d.vercel.app/"
+  );
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
