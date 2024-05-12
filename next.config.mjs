118e5c1b230f9ec88b90b062f2a5ee4a3cf68c44
CristianPerez <norberto.cp@hotmail.com>
2024-05-11 21:49:24 -0300

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  devServer: {
-    // Permitir conexiones desde cualquier host, incluido localhost y 127.0.0.1
-    host: "0.0.0.0",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
