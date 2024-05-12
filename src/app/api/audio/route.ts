72eb44ca947832b91c185204fe35ef9e24c464a0
CristianPerez <norberto.cp@hotmail.com>
2024-05-12 15:57:03 -0300

    access_mode

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -11,48 +11,6 @@ cloudinary.config({
   secure: true,
 });
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const formData = await request.formData();
-//     const file = formData.get("file");
-
-//     if (!file || !(file instanceof File)) {
-//       return NextResponse.json({ error: "No file found in form data" });
-//     }
-
-//     const tempFilePath = path.join(os.tmpdir(), file.name);
-
-//     fs.writeFileSync(tempFilePath, Buffer.from(await file.arrayBuffer()));
-
-//     try {
-//       const result = await cloudinary.uploader.upload(tempFilePath, {
-//         resource_type: "raw",
-//       });
-
-//       fs.unlinkSync(tempFilePath);
-
-//       const response = NextResponse.json({
-//         url: result.secure_url,
-//       });
-
-//       return response;
-//     } catch (error) {
-//       console.error("Error uploading to Cloudinary:", error);
-//       return NextResponse.json({
-//         error: "Error uploading file to Cloudinary",
-//         errorCode: error,
-//         file: file,
-//       });
-//     }
-//   } catch (error) {
-//     console.error("Error uploading to Cloudinary:", error);
-//     return NextResponse.json({
-//       error: "Internal server error",
-//       errorCode: error,
-//     });
-//   }
-// }
-
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.formData();
@@ -69,6 +27,7 @@ export async function POST(request: NextRequest) {
     try {
       const result = await cloudinary.uploader.upload(tempFilePath, {
         resource_type: "raw",
+        access_mode: "public",
       });
 
       fs.unlinkSync(tempFilePath);
@@ -78,12 +37,11 @@ export async function POST(request: NextRequest) {
       });
 
       return response;
-    } catch (uploadError) {
-      console.error("Error uploading to Cloudinary:", uploadError);
+    } catch (error) {
+      console.error("Error uploading to Cloudinary:", error);
       return NextResponse.json({
         error: "Error uploading file to Cloudinary",
-        errorCode: uploadError,
-        file: file,
+        errorCode: error,
       });
     }
   } catch (error) {
