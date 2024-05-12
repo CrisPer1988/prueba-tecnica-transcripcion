623fb96329ff3b380f12d53401567926c518450e
CristianPerez <norberto.cp@hotmail.com>
2024-05-11 23:14:19 -0300

    cambio de nombre env

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -5,9 +5,9 @@ import os from "os";
 import path from "path";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function POST(request: NextRequest) {
