63b4ecb640be07c67574f44553766d117459ebae
CristianPerez <norberto.cp@hotmail.com>
2024-05-12 16:12:06 -0300

    delete secure

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -8,7 +8,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
-  secure: true,
 });
 
 export async function POST(request: NextRequest) {
