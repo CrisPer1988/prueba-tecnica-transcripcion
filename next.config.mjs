dfaaf509a830e305eaf1447266cbb6601fca2313
CristianPerez <norberto.cp@hotmail.com>
2024-05-12 13:13:43 -0300

    new cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-
-          { key: "Access-Control-allow-Origin", value: "*" },
-
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, DELETE, PATCH, POST, PUT",
-          },
-
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "x-CSRF-Token, x-Requested-With, Accept, Accept-Version, Content-Length, Content-MDS, Content-Type, Date, x-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
