def7fc99f02ed32b0c688011751180083a7f725d
CristianPerez <norberto.cp@hotmail.com>
2024-05-12 11:31:41 -0300

    cors configure

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+const allowdOrigins =
+  process.env.NODE_ENV === "production"
+    ? [
+        "https://prueba-tecnica-transcripcion-ibdc53l5o.vercel.app",
+        "https://www.prueba-tecnica-transcripcion-ibdc53l5o.vercel.app",
+      ]
+    : ["http://localhost:3000"];
+
+export default function middlewate(request: Request) {
+  const origin = request.headers.get("origin");
+  console.log(origin);
+
+  if (origin && !allowdOrigins.includes(origin)) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "Bad request",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
