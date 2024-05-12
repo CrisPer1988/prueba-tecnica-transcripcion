2ed419d63e0f2bc64209cc5926e958eabfef0838
CristianPerez <norberto.cp@hotmail.com>
2024-05-12 00:44:55 -0300

    name env

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.API_KEY_CLOUD,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
